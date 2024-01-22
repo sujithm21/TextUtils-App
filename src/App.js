9700bdac5de1e0720ce29dd61384fff523a0546c
sujithm21 <makamsujith2004@gmail.com>
2024-01-22 22:56:02 +0530

    TextUtils app is created using react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import "./App.css";
+import Alert from "./Components/Alert";
+// import About from './Components/About';
+import Navbar from "./Components/Navbar";
+import TextForm from "./Components/TextForm";
+import React, { useState } from "react";
+// import {
+//   BrowserRouter as Router,
+//   RouterProvider,
+//   Routes,
+//   Route
+// } from "react-router-dom";
+// import { BrowserRouter } from 'react-router-dom'
 
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const [alert, setAlert] = useState("hi");
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    })
+
+    // setTimeout(() => {
+    //   setAlert(null)
+    // },1500)
+
+  }
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "grey";
+      showAlert("Dark mode has been enabled","success");
+      document.title = 'Text Utils - Dark Mode'
+
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled","success");
+      document.title = 'Text Utils -Light Mode'
+    } 
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Router>   */}
+      <Navbar
+        title="TextUtils"
+        about="Link"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+
+      <Alert alert={alert} />
+
+      <TextForm showAlert = {showAlert} heading="Enter your text" mode={mode} />
+
+      <div className="container my-3">
+        {/* <About/>  */}
+        
+        {/* <Routes>
+                <Route path="/about" element = {<About />} />
+                  
+                <Route path="/" elemnt  = {<TextForm showAlert = {showAlert} heading="Enter your text" mode={mode} />} />
+
+        </Routes>  */}
+      </div>
+      {/* </Router> */}
+    </>
   );
 }
 
---
+++ src/Components/TextForm.js
@@ -0,0 +1,55 @@
+import React, {useState}from 'react'
+
+export default function TextForm(props) {
+
+    const handleUpclick = ()=>{
+       // console.log("Upcase button clicked")
+        let newText = text.toUpperCase()
+        settext(newText)
+        props.showAlert("Converted to upper case!","success");
+    }
+
+    const handleonChange = (event)=>{
+        console.log("On change")
+        settext(event.target.value)
+    }
+
+    const handleLowclick = ()=>{
+        // console.log("Upcase button clicked")
+         let newText = text.toLowerCase()
+         settext(newText)
+         props.showAlert("Converted to lower case!","success");
+    }
+
+    const speak = () => {
+        let msg = new SpeechSynthesisUtterance();
+        msg.text = text;
+        window.speechSynthesis.speak(msg);
+    }
+
+  const[text,settext] = useState('');
+  return (
+    <>
+    <div className='container my-3' style = {{color:props.mode === 'light'? 'white':'grey'}}>
+        
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+        <textarea className="form-control" value ={text} onChange = {handleonChange} style={{backgroundColor :props.mode === 'light'? 'white':'grey',color :props.mode === 'light'? 'grey':'white'}} id="mybox" rows="7"></textarea>
+        </div>
+        <button className="btn btn-primary mx-3" onClick = {handleUpclick}>Convert to Uppercase</button>
+        <button className="btn btn-primary" onClick = {handleLowclick}>Convert to Lowercase</button>
+        <button type="submit" onClick={speak} className="btn btn-warning mx-2 my-2">Speak</button>
+    </div>
+
+    <div className='container mb-6' >
+        <h2>Your Text Summary</h2>
+        <p>{text.split(" ").length}words and {text.length}characters </p>
+        <p>It will take {0.008 * text.split(" ").length} Minutes to read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+
+    </>
+    
+  )
+}
